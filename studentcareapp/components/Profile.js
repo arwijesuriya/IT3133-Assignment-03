1b0719e2c7289d912c1d231dede7dd0179168c51
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 10:10:44 +0000

    Added styles and banner to Sub Components

--- studentcareapp/components/Profile.js
+++ studentcareapp/components/Profile.js
@@ -1,7 +1,40 @@
-import { View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
-export default function Profile(){
+
+export default function Profile({user}){
     return(
-        <View><Text>This is Profile Screen</Text></View>
+        <View style={styles.container}>
+            <View style={styles.banner}>
+                <Image
+                    style={styles.image}
+                    source={require('../assets/Logo.png')}/>
+            </View>
+            <View style={styles.imageCard}>
+                <Text>This is Profile Screen {user.name}</Text>
+            </View>
+        </View>
     )
 }
+const styles =StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+    },
+    banner:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+    },  
+    image:{
+        flex:1,
+        width:250,
+        height:65,
+        top:10,
+        position:'absolute',
+    },
+    imageCard:{
+        flex:2
+    }
+});
