221b7b908c089b816fc19b933235963bd8247772
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 10:38:39 +0000

    Created the content for Course

--- studentcareapp/components/Courses.js
+++ studentcareapp/components/Courses.js
@@ -9,7 +9,25 @@ export default function Courses({user}){
                     source={require('../assets/Logo.png')}/>
             </View>
             <View style={styles.imageCard}>
-                <Text>This is Courses Screen</Text>
+                <Card style={{backgroundColor:"#ffffff"}}>
+                    <Card.Content>
+                        <Text variant="titleLarge" style={styles.nametext}>
+                            {userCourse.name}
+                        </Text>
+                        <Text variant="bodyMedium" style={{textAlign:'center'}}>
+                            Code: {userCourse.course_code} | Dept: {userCourse.department}
+                        </Text>
+                        <View style={styles.divider} />
+                        <Text variant="titleLarge" style={styles.cardtitletext}>
+                            Course Information
+                        </Text>
+                        <Text variant="bodyMedium">Code: {userCourse.course_code}</Text>
+                        <Text variant="bodyMedium">Department: {userCourse.department}</Text>
+                        <Text variant="bodyMedium">Duration: {userCourse.duration}</Text>
+                        <Text variant="bodyMedium">Description: {userCourse.description}</Text>
+                        <View style={styles.divider} />
+                    </Card.Content>
+                </Card>
             </View>
             <View style={styles.footer}>
                 <Text style={styles.footerText}>UOV © 2025</Text>
