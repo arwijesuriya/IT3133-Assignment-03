6f43b3b8605051ae5ffb53844f9f86e721c786d9
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 09:41:24 +0000

    Added header styles

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -1,21 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Login from './components/Login';
+import Main from './components/Main';
+import { StyleSheet } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
-  
   const Stack = createNativeStackNavigator();
-  
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='UoV Student Care'>
-          <Stack.Screen name='UOV Student Care' component={Login} />    
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Login'>
+            <Stack.Screen name='Login' component={Login} options={{title:'UoV Student Care',headerStyle: { backgroundColor: '#520f4e' },headerTintColor: '#fff'}} />    
+            <Stack.Screen name='Main' component={Main} options={{title:'UoV Student Care',headerStyle: { backgroundColor: '#520f4e' },headerTintColor: '#fff'}}/>    
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
+const styles=StyleSheet.create({
+  image:{
+    width:500
+  }
+})
