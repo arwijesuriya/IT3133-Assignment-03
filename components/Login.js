eea110c0a6f7f59eb4d3fe845adb331522b6a8d4
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 09:19:03 +0000

    create Login.js

---
+++ components/Login.js
@@ -0,0 +1,36 @@
+import { useNavigation } from "@react-navigation/native";
+import { Button, PaperProvider, Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+
+export default function Login() {
+    const navigation = useNavigation();
+    return (
+        <PaperProvider>
+            <View style={styles.container}>
+                <Text style={styles.text}>Student Login</Text>
+                <Button mode="contained"
+                    onPress={() => navigation.navigate('Main')}
+                    labelStyle={styles.buttonText}
+                >
+                    Login
+                </Button>
+            </View>
+        </PaperProvider>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+    },
+    text: {
+        color: "black",
+        fontSize: 20,
+        marginBottom: 20,
+    },
+    buttonText: {
+        color: "black",
+    },
+});
