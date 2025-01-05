7b7313dc8c68b84b6ae8f93ce9ab6db1d943ed3c
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 10:05:37 +0000

    Created Profile, Courses, Subject components

---
+++ studentcareapp/components/Courses.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+export default function Courses(){
+    return(
+        <View><Text>This is Courses Screen</Text></View>
+    )
+}
---
+++ studentcareapp/components/Profile.js
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+export default function Profile(){
+    return(
+        <View><Text>This is Profile Screen</Text></View>
+    )
+}
