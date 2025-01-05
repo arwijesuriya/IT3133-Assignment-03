b22befcf3e4773b1b0319ee0269f31a92c06d188
Achira Wijesuriya <129876238+arwijesuriya@users.noreply.github.com>
2025-01-05 09:08:49 +0000

    login on app.js structure

--- studentcareapp/App.js
+++ studentcareapp/App.js
@@ -1,20 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Login from './components/Login';
 
 export default function App() {
+  
+  const Stack = createNativeStackNavigator();
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='UoV Student Care'>
+          <Stack.Screen name='UOV Student Care' component={Login} />    
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
